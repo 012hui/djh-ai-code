8a639ed2c2a11f066f719da1e44c48925c97feca
012hui <2833353159@qq.com>
2026-03-30 22:56:00 +0800

    引入SSE流式输出，LangChain4j + Reactor，响应式编程，把AI返回内容封装成Flux响应式对象处理。  流式返回过程中拼接AI结果，可以实时返回给前端，全部输出后，再进行解析和保存。

--- src/main/java/org/test/djhaicode/core/AiCodeGeneratorFacade.java
+++ src/main/java/org/test/djhaicode/core/AiCodeGeneratorFacade.java
@@ -1,6 +1,7 @@
 package org.test.djhaicode.core;
 
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.test.djhaicode.ai.AiCodeGeneratorService;
 import org.test.djhaicode.ai.model.HtmlCodeResult;
@@ -8,12 +9,14 @@ import org.test.djhaicode.ai.model.MultiFileCodeResult;
 import org.test.djhaicode.exception.BusinessException;
 import org.test.djhaicode.exception.ErrorCode;
 import org.test.djhaicode.model.enums.CodeGenTypeEnum;
+import reactor.core.publisher.Flux;
 
 import java.io.File;
 
 /**
  * AI 代码生成外观类，组合生成和保存功能
  */
+@Slf4j
 @Service
 public class AiCodeGeneratorFacade {
 
@@ -40,6 +43,87 @@ public class AiCodeGeneratorFacade {
             }
         };
     }
+
+    /**
+     * 统一入口：根据类型生成并保存代码（使用门面模式）（流式）
+     *
+     * @param userMessage     用户提示词
+     * @param codeGenTypeEnum 生成类型
+     * @return 保存的目录
+     */
+    public Flux<String> generateAndSaveCodeStream(String userMessage, CodeGenTypeEnum codeGenTypeEnum) {
+        if (codeGenTypeEnum == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "生成类型为空");
+        }
+        return switch (codeGenTypeEnum) {
+            case HTML -> generateAndSaveHtmlCodeStream(userMessage);
+            case MULTI_FILE -> generateAndSaveMultiFileCodeStream(userMessage);
+            default -> {
+                String errorMessage = "不支持的生成类型：" + codeGenTypeEnum.getValue();
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR, errorMessage);
+            }
+        };
+    }
+
+    /**
+     * 生成 HTML 模式的代码并保存（流式）
+     *
+     * @param userMessage 用户提示词
+     * @return 保存的目录
+     */
+    private Flux<String> generateAndSaveHtmlCodeStream(String userMessage) {
+        //Flux表示一个0-N个元素的异步序列，这里每个元素是代码的字符串片段
+        Flux<String> result = aiCodeGeneratorService.generateHtmlCodeStream(userMessage);
+        //可变的字符串容器,字符串拼接器，用于当流式返回所有的代码之后，再保存代码
+        StringBuilder codeBuilder = new StringBuilder();
+        //doOnNext方法的作用：注册一个回调，在每个元素（代码片段）从流中发出时执行
+        //参数：chunk是每个代码片段（字符串）
+        return result.doOnNext(chunk->{
+            //实时收集代码片段,将每个代码片段追加到 codeBuilder中
+            codeBuilder.append(chunk);
+        }).doOnComplete(()->{//doOnComplete方法：注册一个回调，在流正常完成时执行
+            //流式返回完成后，保存代码
+            try{
+                String completeHtmlCode = codeBuilder.toString();
+                //解析代码为对象
+                HtmlCodeResult htmlCodeResult = CodeParser.parseHtmlCode(completeHtmlCode);
+                //代码保存到文件
+                File saveDir = CodeFileSaver.saveHtmlCodeResult(htmlCodeResult);
+                log.info("保存成功，目录为：{}",saveDir.getAbsolutePath());
+            }catch (Exception e){
+                log.error("保存失败：{}",e.getMessage());
+            }
+        });
+    }
+
+    /**
+     * 生成多文件模式的代码并保存（流式）
+     *
+     * @param userMessage 用户提示词
+     * @return 保存的目录
+     */
+    private Flux<String> generateAndSaveMultiFileCodeStream(String userMessage) {
+        Flux<String> result = aiCodeGeneratorService.generateMultiFileCodeStream(userMessage);
+        //字符串拼接器，用于当流式返回所有的代码之后，再保存代码
+        StringBuilder codeBuilder = new StringBuilder();
+        return result.doOnNext(chunk->{
+            //实时收集代码片段
+            codeBuilder.append(chunk);
+        }).doOnComplete(()->{
+            //流式返回完成后，保存代码
+            try{
+                String completeMultiFileCode = codeBuilder.toString();
+                //解析代码为对象
+                MultiFileCodeResult multiFileCodeCodeResult = CodeParser.parseMultiFileCode(completeMultiFileCode);
+                //代码保存到文件
+                File saveDir = CodeFileSaver.saveMultiFileCodeResult(multiFileCodeCodeResult);
+                log.info("保存成功，目录为：{}",saveDir.getAbsolutePath());
+            }catch (Exception e){
+                log.error("保存失败：{}",e.getMessage());
+            }
+        });
+    }
+
 
     /**
      * 生成 HTML 模式的代码并保存
