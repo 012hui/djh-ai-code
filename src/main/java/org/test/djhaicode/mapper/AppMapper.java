38c0b1d3de548edb4454b0833606ddbae1f25912
012hui <2833353159@qq.com>
2026-04-01 21:49:13 +0800

    后端应用模块： -【用户】创建应用（须填写 initPrompt） -【用户】根据 id 修改自己的应用（目前只支持修改应用名称） -【用户】根据 id 删除自己的应用 -【用户】根据 id 查看应用详情 -【用户】分页查询自己的应用列表（支持根据名称查询，每页最多 20 个） -【用户】分页查询精选的应用列表（支持根据名称查询，每页最多 20 个） -【管理员】根据 id 删除任意应用 -【管理员】根据 id 更新任意应用（支持更新应用名称、应用封面、优先级） -【管理员】分页查询应用列表（支持根据除时间外的任何字段查询，每页数量不限） -【管理员】根据 id 查看应用详情

--- src/main/java/org/test/djhaicode/mapper/AppMapper.java
+++ src/main/java/org/test/djhaicode/mapper/AppMapper.java
@@ -1,6 +1,7 @@
 package org.test.djhaicode.mapper;
 
 import com.mybatisflex.core.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.test.djhaicode.model.entity.App;
 
 /**
@@ -8,6 +9,7 @@ import org.test.djhaicode.model.entity.App;
  *
  * @author <a href="https://github.com/012hui">djh</a>
  */
+@Mapper
 public interface AppMapper extends BaseMapper<App> {
 
 }
--- src/main/java/org/test/djhaicode/service/AppService.java
+++ src/main/java/org/test/djhaicode/service/AppService.java
@@ -1,7 +1,12 @@
 package org.test.djhaicode.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import org.test.djhaicode.model.dto.app.AppQueryRequest;
 import org.test.djhaicode.model.entity.App;
+import org.test.djhaicode.model.vo.AppVO;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +15,24 @@ import org.test.djhaicode.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     *获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    /**
+     * 获取应用封装类列表
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
 }
